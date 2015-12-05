b138d21a96aec01ad9926e286cd216ee94f1d0ff
ARMmaster17 <wordman05@gmail.com>
2015-12-05 16:42:34 -0500

    Basic camera functionality

--- BlockWars/BlockWars/Game1.cs
+++ BlockWars/BlockWars/Game1.cs
@@ -19,6 +19,27 @@ namespace BlockWars
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        bool showDebug = false;
+
+        private SpriteFont sf_menuitem;
+        private SpriteFont sf_debug;
+
+        static Vector3 avatarPosition = new Vector3(0, 0, 3);
+        static Vector3 cameraPosition = avatarPosition;
+
+        VertexBuffer vertexBuffer;
+
+        BasicEffect basicEffect;
+        Matrix world = Matrix.CreateTranslation(0, 0, 0);
+        Matrix view = Matrix.CreateLookAt(cameraPosition, new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+        Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.01f, 100f);
+
+        float updownRot = 0.0f;
+        float leftrightRot = 0.0f;
+        float rotationSpeed = 0.5f;
+
+        MouseState originalMouseState;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -47,7 +68,21 @@ namespace BlockWars
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            sf_menuitem = Content.Load<SpriteFont>("MenuItem");
+            sf_debug = Content.Load<SpriteFont>("Debug");
+
+            basicEffect = new BasicEffect(GraphicsDevice);
+
+            VertexPositionColor[] vertices = new VertexPositionColor[3];
+            vertices[0] = new VertexPositionColor(new Vector3(0, 1, 0), Color.Red);
+            vertices[1] = new VertexPositionColor(new Vector3(+0.5f, 0, 0), Color.Green);
+            vertices[2] = new VertexPositionColor(new Vector3(-0.5f, 0, 0), Color.Blue);
+
+            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 3, BufferUsage.WriteOnly);
+            vertexBuffer.SetData<VertexPositionColor>(vertices);
+
+            Mouse.SetPosition(5, 5);
+            originalMouseState = Mouse.GetState();
         }
 
         /// <summary>
@@ -71,7 +106,21 @@ namespace BlockWars
             {
                 this.Exit();
             }
-
+            // Check for debug enable
+            if (Keyboard.GetState().IsKeyDown(Keys.F1))
+            {
+                showDebug = true;
+                if (showDebug)
+                {
+                    showDebug = false;
+                }
+                else
+                {
+                    showDebug = true;
+                }
+            }
+            float timeDifference = (float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000.0f;
+            ProcessInput(timeDifference);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -83,11 +132,80 @@ namespace BlockWars
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
+            UpdateViewMatrix();
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            basicEffect.World = world;
+            basicEffect.View = view;
+            basicEffect.Projection = projection;
+            basicEffect.VertexColorEnabled = true;
+
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
+            }
+            
+            spriteBatch.Begin();
+            spriteBatch.DrawString(sf_debug, cameraPosition.ToString(), new Vector2(5, 5), Color.Black);
+            spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+        private void UpdateViewMatrix()
+        {
+            Matrix cameraRotation = Matrix.CreateRotationX(updownRot) * Matrix.CreateRotationY(leftrightRot);
+
+            Vector3 cameraOriginalTarget = new Vector3(0, 0, -1);
+            Vector3 cameraRotatedTarget = Vector3.Transform(cameraOriginalTarget, cameraRotation);
+            Vector3 cameraFinalTarget = cameraPosition + cameraRotatedTarget;
+
+            Vector3 cameraOriginalUpVector = new Vector3(0, 1, 0);
+            Vector3 cameraRotatedUpVector = Vector3.Transform(cameraOriginalUpVector, cameraRotation);
+
+            view = Matrix.CreateLookAt(cameraPosition, cameraFinalTarget, cameraRotatedUpVector);
+        }
+        private void ProcessInput(float amount)
+        {
+            MouseState currentMouseState = Mouse.GetState();
+            if (currentMouseState != originalMouseState)
+            {
+                float xDifference = currentMouseState.X - originalMouseState.X;
+                float yDifference = currentMouseState.Y - originalMouseState.Y;
+                leftrightRot -= rotationSpeed * xDifference * amount;
+                updownRot -= rotationSpeed * yDifference * amount;
+                Mouse.SetPosition(5, 5);
+                UpdateViewMatrix();
+            }
+            
+            Vector3 moveVector = new Vector3(0, 0, 0);
+            KeyboardState keyState = Keyboard.GetState();
+            if (keyState.IsKeyDown(Keys.Up) || keyState.IsKeyDown(Keys.W))
+                moveVector += new Vector3(0, 0, -1);
+            if (keyState.IsKeyDown(Keys.Down) || keyState.IsKeyDown(Keys.S))
+                moveVector += new Vector3(0, 0, 1);
+            if (keyState.IsKeyDown(Keys.Right) || keyState.IsKeyDown(Keys.D))
+                moveVector += new Vector3(1, 0, 0);
+            if (keyState.IsKeyDown(Keys.Left) || keyState.IsKeyDown(Keys.A))
+                moveVector += new Vector3(-1, 0, 0);
+            if (keyState.IsKeyDown(Keys.Q))
+                moveVector += new Vector3(0, 1, 0);
+            if (keyState.IsKeyDown(Keys.Z))
+                moveVector += new Vector3(0, -1, 0);
+            AddToCameraPosition(moveVector * amount);
+        }
+        private void AddToCameraPosition(Vector3 vectorToAdd)
+        {
+            Matrix cameraRotation = Matrix.CreateRotationX(updownRot) * Matrix.CreateRotationY(leftrightRot);
+            Vector3 rotatedVector = Vector3.Transform(vectorToAdd, cameraRotation);
+            cameraPosition += /*moveSpeed */ rotatedVector;
+            UpdateViewMatrix();
         }
     }
 }
